09514a9f30caa183f2fe31bd92d548cb26edd790
arhaanb <arhaanb@gmail.com>
2023-12-08 19:27:32 +0530

    add game over screen + revive

--- src/main/java/com/arhaanb/stickhero/stickhero/PlayController.java
+++ src/main/java/com/arhaanb/stickhero/stickhero/PlayController.java
@@ -47,10 +47,19 @@ public class PlayController {
   private AnchorPane pane;
 
   @FXML
+  private AnchorPane over_pane;
+
+  @FXML
   private Button closebtn;
 
   @FXML
-  private Text scoretext;
+  private Text scoretext; // top bar text
+
+  @FXML
+  private Text scorenum; // game over score text
+
+  @FXML
+  private Text total_cherries; // game over total cherries
 
   @FXML
   private Text cherrytext;
@@ -80,8 +89,246 @@ public class PlayController {
 
   boolean ignoreClick = false;
 
-  public void updateText(String newText) {
-    scoretext.setText(newText);
+  public void updateText(String newText, Text where) {
+    where.setText(newText);
+  }
+
+  @FXML
+  public void revive() {
+    System.out.println("reviving player");
+
+    mouseHoldEnabled = true;
+
+    //reset player position
+    // sprite.setX(rects.get(rects.size() - 1).getBoundsInLocal().getMinX());
+    // sprite.setX(0);
+
+    attachMouseHandlers();
+
+    // pane.setOnMousePressed(event -> {
+    //   System.out.println("bro?");
+    // });
+
+    TranslateTransition undoFall = new TranslateTransition(
+      Duration.millis(100),
+      sprite
+    );
+
+    undoFall.setByY(-400 - sprite.getY()); // Set to the negative of the previous translation
+    undoFall.setToX(rects.get(rects.size() - 2).getX() - sprite.getX()); // Set to the negative of the previous translation
+
+    undoFall.setOnFinished(afterUndo -> {
+      updateText(Integer.toString(cherries - 2), cherrytext);
+
+      line.setStartX(
+        rects.get(rects.size() - 2).getX() +
+        (rects.get(rects.size() - 2).getWidth() / 4) *
+        3
+      );
+      line.setEndX(
+        rects.get(rects.size() - 2).getX() +
+        (rects.get(rects.size() - 2).getWidth() / 4) *
+        3
+      );
+
+      line.setStartY(234.5);
+      line.setEndY(234.5);
+
+      //minus 2 cherries
+      cherries -= 2;
+      over_pane.setVisible(false);
+    });
+
+    // To play the undo animation, call play() on undoFall
+    // For example, if you want to undo the translation after a certain condition is met:
+    undoFall.play();
+  }
+
+  private void attachMouseHandlers() {
+    pane.setOnMousePressed(event -> {
+      if (event.getButton() == MouseButton.PRIMARY) {
+        System.out.println("heloleolerol");
+        startTime = System.currentTimeMillis();
+        if (mouseHoldEnabled) {
+          // addRectangle();
+          System.out.println("Mouse clicked");
+
+          startTime = System.currentTimeMillis();
+          isMouseDown = true;
+
+          if (timer == null) {
+            timer = new Timer();
+            timer.scheduleAtFixedRate(
+              new TimerTask() {
+                @Override
+                public void run() {
+                  if (isMouseDown) {
+                    double elapsedTime = System.currentTimeMillis() - startTime;
+                    double lineHeight = elapsedTime * 0.01; // Adjust this factor to control the height growth rate
+
+                    line.setEndY(line.getEndY() - lineHeight);
+                  }
+                }
+              },
+              0,
+              10
+            );
+          }
+          System.out.println("blockNum: " + blockNum);
+        }
+      }
+    });
+
+    pane.setOnMouseClicked(event -> {
+      if (event.getButton() == MouseButton.SECONDARY) {
+        double currentScaleY = sprite.getScaleY();
+
+        System.out.println(sprite.getY());
+        if (currentScaleY == 1) {
+          sprite.setY(sprite.getFitHeight());
+          sprite.setScaleY(-1);
+        } else {
+          sprite.setY(0.0);
+          sprite.setScaleY(1);
+        }
+      }
+    });
+
+    pane.setOnMouseReleased(event -> {
+      if (event.getButton() == MouseButton.PRIMARY) {
+        ignoreClick = System.currentTimeMillis() - startTime < 100;
+        if (mouseHoldEnabled) {
+          mouseHoldEnabled = false;
+          isMouseDown = false;
+          timer.cancel();
+          timer = null;
+
+          // rotating the stick
+          double angle = Math.toRadians(90.0); // Angle of rotation in degrees
+
+          // Calculate the offsets from the start point
+          double dx = line.getEndX() - line.getStartX();
+          double dy = line.getEndY() - line.getStartY();
+
+          // Rotate the end point around the start point
+          double newEndX =
+            line.getStartX() + dx * Math.cos(angle) - dy * Math.sin(angle);
+          double newEndY =
+            line.getStartY() + dx * Math.sin(angle) + dy * Math.cos(angle);
+
+          // Update the line's end coordinates
+          line.setEndX(newEndX);
+          line.setEndY(newEndY);
+
+          // System.out.println("line end: " + line.getEndX());
+          // System.out.println(rects.get(blockNum + 1).getX());
+          // System.out.println(
+          //   rects.get(blockNum + 1).getX() + rects.get(blockNum + 1).getWidth()
+          // );
+
+          if (
+            line.getEndX() > rects.get(rects.size() - 1).getX() &&
+            line.getEndX() <
+            rects.get(rects.size() - 1).getX() +
+            rects.get(rects.size() - 1).getWidth()
+          ) {
+            walking = true;
+
+            double newX = rects.get(rects.size() - 1).getX();
+            double translationXbro = newX - sprite.getX();
+
+            TranslateTransition transition = new TranslateTransition(
+              Duration.millis(750),
+              sprite
+            );
+
+            transition.setToX(translationXbro);
+
+            Timeline positionPrintTimeline = new Timeline(
+              new KeyFrame(
+                Duration.millis(100),
+                posevent -> {
+                  if (
+                    checkCherryCollision(sprite, cherryView) &&
+                    sprite.getScaleY() == -1
+                  ) {
+                    System.out.println("cherry collected");
+                    cherryCollected = true;
+                    pane.getChildren().remove(cherryView);
+                  }
+                }
+              )
+            );
+
+            positionPrintTimeline.setCycleCount(Timeline.INDEFINITE);
+
+            // Node nodeToExclude = closebtn;
+
+            transition.setOnFinished(boo -> {
+              blockNum += 1;
+
+              if (cherryCollected) {
+                cherryCollected = false;
+                cherries += 1;
+                cherrytext.setText("CHERRIES: " + cherries);
+              }
+              walking = false;
+              positionPrintTimeline.stop();
+              if (sprite.getScaleY() == -1) {
+                try {
+                  dieded();
+                } catch (IOException e) {
+                  e.printStackTrace();
+                }
+              } else {
+                score++;
+                updateText("SCORE: " + score, scoretext);
+
+                // reset stick
+                line.setStartX(
+                  rects.get(blockNum).getX() +
+                  (rects.get(blockNum).getWidth() / 4) *
+                  3
+                );
+                line.setEndX(
+                  rects.get(blockNum).getX() +
+                  (rects.get(blockNum).getWidth() / 4) *
+                  3
+                );
+
+                line.setStartY(234.5);
+                line.setEndY(234.5);
+
+                addRectangle();
+
+                TranslateTransition movePane = new TranslateTransition(
+                  Duration.seconds(0.5),
+                  pane
+                );
+
+                double targetX = rects.get(blockNum).getX() - 100;
+                movePane.setToX(-targetX);
+                movePane.setOnFinished(finishedEvent -> {
+                  mouseHoldEnabled = true;
+                });
+
+                movePane.play();
+              }
+            });
+            // Play the animation
+            transition.play();
+            positionPrintTimeline.play();
+          } else {
+            //dies
+            try {
+              dieded();
+            } catch (IOException e) {
+              e.printStackTrace();
+            }
+          }
+        }
+      }
+    });
   }
 
   private boolean checkCherryCollision(ImageView player, ImageView fruit) {
@@ -155,27 +402,13 @@ public class PlayController {
     double translationY = newY - sprite.getY();
 
     fall.setByY(translationY);
-    // fall.setOnFinished(onfallover -> {
-    //   try {
-    //     FXMLLoader loader = new FXMLLoader(
-    //       getClass().getResource("game_over.fxml")
-    //     );
-    //     Parent root = loader.load();
 
-    //     GameOverController gameOverController = loader.getController();
-    //     gameOverController.setScore(score);
+    fall.setOnFinished(afterFall -> {
+      updateText(Integer.toString(score), scorenum);
 
-    //     stage = (Stage) pane.getScene().getWindow();
-    //     stage.setScene(new Scene(root));
-    //     stage.sizeToScene();
-    //     stage.show();
-    //   } catch (IOException e) {
-    //     // Handle the IOException as needed
-    //     e.printStackTrace();
-    //   }
-    // });
+      over_pane.setVisible(true);
+    });
 
-    // Set the event handler to start the second transition when the first one is finished
     transition.setOnFinished(moveTransOver -> {
       fall.play();
     });
@@ -187,6 +420,7 @@ public class PlayController {
   public void initialize() {
     Rectangle firstRectangle = new Rectangle(40, 437, 100, 283.0);
     sprite.toFront();
+    System.out.println(sprite.getY());
 
     // System.out.println("line.setStartX(" + line.getStartX() + ");");
     // System.out.println("line.setEndX(" + line.getEndX() + ");");
@@ -195,246 +429,21 @@ public class PlayController {
 
     Image image = new Image(getClass().getResourceAsStream("cherry.png"));
     cherryView = new ImageView(image);
-    cherryView.setFitWidth(50); // Set the width of the image
-    cherryView.setFitHeight(50); // Set the height of the image
+    cherryView.setFitWidth(50);
+    cherryView.setFitHeight(50);
     cherryView.setY(445);
 
-    // Add imageView to your layout (e.g., a Pane)
     pane.getChildren().add(cherryView);
 
-    // Add the rectangles to the AnchorPane
     pane.getChildren().add(firstRectangle);
 
-    // Set the X position of the sprite
     sprite.setX(40);
     rects.add(firstRectangle);
 
     addRectangle();
 
-    pane.setOnMousePressed(event -> {
-      if (event.getButton() == MouseButton.PRIMARY) {
-        startTime = System.currentTimeMillis();
-        if (mouseHoldEnabled) {
-          // addRectangle();
-          System.out.println("Mouse clicked");
-
-          startTime = System.currentTimeMillis();
-          isMouseDown = true;
-
-          if (timer == null) {
-            timer = new Timer();
-            timer.scheduleAtFixedRate(
-              new TimerTask() {
-                @Override
-                public void run() {
-                  if (isMouseDown) {
-                    double elapsedTime = System.currentTimeMillis() - startTime;
-                    double lineHeight = elapsedTime * 0.01; // Adjust this factor to control the height growth rate
-
-                    line.setEndY(line.getEndY() - lineHeight);
-                  }
-                }
-              },
-              0,
-              10
-            ); // Adjust the delay and period to control the update rate
-          }
-          blockNum += 1;
-          System.out.println("blockNum: " + blockNum);
-        }
-      }
-    });
-
-    pane.setOnMouseClicked(event -> {
-      if (event.getButton() == MouseButton.SECONDARY) {
-        System.out.println("mouse click event");
-        // Assuming sprite is an ImageView or a subclass of Node
-        double currentScaleY = sprite.getScaleY();
-
-        System.out.println(sprite.getY());
-        if (currentScaleY == 1) {
-          sprite.setY(sprite.getFitHeight());
-          sprite.setScaleY(-1);
-        } else {
-          sprite.setY(0.0);
-          sprite.setScaleY(1);
-        }
-        // double spriteHeight = sprite.getBoundsInLocal().getHeight();
-
-        // // Set the pivot point at the bottom edge of the image
-        // sprite.setTranslateY(sprite.getTranslateY() + spriteHeight);
-
-        // // Check the current scaleY
-
-        // // Toggle between 1 and -1
-        // sprite.setScaleY(currentScaleY == 1 ? -1 : 1);
-
-        // // Reset the pivot point to the original position
-        // sprite.setTranslateY(sprite.getTranslateY() - spriteHeight);
-      }
-    });
-
-    pane.setOnMouseReleased(event -> {
-      if (event.getButton() == MouseButton.PRIMARY) {
-        ignoreClick = System.currentTimeMillis() - startTime < 100;
-        if (mouseHoldEnabled) {
-          mouseHoldEnabled = false;
-          isMouseDown = false;
-          timer.cancel();
-          timer = null;
-
-          // rotating the stick
-          double angle = Math.toRadians(90.0); // Angle of rotation in degrees
-
-          // Calculate the offsets from the start point
-          double dx = line.getEndX() - line.getStartX();
-          double dy = line.getEndY() - line.getStartY();
-
-          // Rotate the end point around the start point
-          double newEndX =
-            line.getStartX() + dx * Math.cos(angle) - dy * Math.sin(angle);
-          double newEndY =
-            line.getStartY() + dx * Math.sin(angle) + dy * Math.cos(angle);
-
-          // Update the line's end coordinates
-          line.setEndX(newEndX);
-          line.setEndY(newEndY);
-
-          // System.out.println("line end: " + line.getEndX());
-          // System.out.println(rects.get(blockNum + 1).getX());
-          // System.out.println(
-          //   rects.get(blockNum + 1).getX() + rects.get(blockNum + 1).getWidth()
-          // );
-
-          if (
-            line.getEndX() > rects.get(blockNum).getX() &&
-            line.getEndX() <
-            rects.get(blockNum).getX() +
-            rects.get(blockNum).getWidth()
-          ) {
-            walking = true;
-
-            double newX = rects.get(blockNum).getX();
-            double translationXbro = newX - sprite.getX();
-
-            // Calculate the duration based on the distance and speed
-            // double durationMillis = 500.0; // Fixed duration in milliseconds
-
-            // Calculate speed based on the fixed duration
-            // double speed = Math.abs(translationXbro) / durationMillis;
-
-            TranslateTransition transition = new TranslateTransition(
-              Duration.millis(750),
-              sprite
-            );
-
-            // TranslateTransition transition = new TranslateTransition(
-            //   Duration.millis(translationXbro / 0.3),
-            //   sprite
-            // );
-
-            transition.setToX(translationXbro);
-
-            Timeline positionPrintTimeline = new Timeline(
-              new KeyFrame(
-                Duration.millis(100),
-                posevent -> {
-                  if (
-                    checkCherryCollision(sprite, cherryView) &&
-                    sprite.getScaleY() == -1
-                  ) {
-                    System.out.println("cherry collected");
-                    cherryCollected = true;
-                    pane.getChildren().remove(cherryView);
-                  }
-                }
-              )
-            );
-
-            positionPrintTimeline.setCycleCount(Timeline.INDEFINITE);
-
-            // Node nodeToExclude = closebtn;
-
-            transition.setOnFinished(boo -> {
-              if (cherryCollected) {
-                cherryCollected = false;
-                cherries += 1;
-                cherrytext.setText("CHERRIES: " + cherries);
-              }
-              positionPrintTimeline.stop();
-              if (sprite.getScaleY() == -1) {
-                try {
-                  dieded();
-                } catch (IOException e) {
-                  e.printStackTrace();
-                }
-              } else {
-                walking = false;
-                score++;
-                updateText("SCORE: " + score);
-                line.setStartX(
-                  rects.get(blockNum).getX() +
-                  (rects.get(blockNum).getWidth() / 4) *
-                  3
-                );
-                line.setEndX(
-                  rects.get(blockNum).getX() +
-                  (rects.get(blockNum).getWidth() / 4) *
-                  3
-                );
-                line.setStartY(234.5);
-                line.setEndY(234.5);
-                addRectangle();
-
-                TranslateTransition movePane = new TranslateTransition(
-                  Duration.seconds(0.5),
-                  pane
-                );
-                // for (Node node : pane.getChildren()) {
-                //   if (node != nodeToExclude) {
-                double targetX = rects.get(blockNum).getX() - 100;
-                movePane.setToX(-targetX);
-                movePane.setOnFinished(finishedEvent -> {
-                  // Enable mouse events after the transition is finished
-                  mouseHoldEnabled = true;
-                });
-
-                movePane.play();
-              }
-            });
-            // Play the animation
-            transition.play();
-            positionPrintTimeline.play();
-          } else {
-            //dies
-            try {
-              dieded();
-            } catch (IOException e) {
-              e.printStackTrace();
-            }
-          }
-        }
-      }
-    });
+    attachMouseHandlers();
   }
-
-  // public void initialize() {
-  //   double lineHeight = 200.0;
-  //   // Create an animation object
-  //   Timeline animation = new Timeline(
-  //     new KeyFrame(
-  //       Duration.ZERO,
-  //       new KeyValue(line.endYProperty(), line.getEndY())
-  //     ),
-  //     new KeyFrame(
-  //       Duration.millis(1000),
-  //       new KeyValue(line.endYProperty(), line.getEndY() - lineHeight)
-  //     )
-  //   );
-
-  //   // Play the animation
-  //   animation.play();
-  // }
 
   @FXML
   public void switchToPlay(ActionEvent event) throws IOException {
